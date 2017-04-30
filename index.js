50cc53f25ef2ee8510b8e19e5d69851bc54f1cc3
842Mono <842mono@gmail.com>
2017-04-30 02:31:22 +0200

    remove 200

--- index.js
+++ index.js
@@ -174,7 +174,15 @@ app.post
 				{
 					let text = event.message.text
 
-
+					for (var i = 0; i < GREETING_KEYWORDS.length; i++)
+					{
+						if (text.indexOf(GREETING_KEYWORDS[i]) >= 0)
+						{
+							var rand = GREETING_RESPONSES[Math.floor(Math.random() * GREETING_RESPONSES.length)];
+							sendTextMessage(sender, rand, token);
+						}
+					}
+					
 						if (text === 'Generic')
 						{
 							sendGenericMessage(sender)
@@ -223,7 +231,6 @@ app.post
 		catch(err)
 		{
 			sendTextMessage(sender,err.message, token);
-			res.sendStatus(200);
 		}
 	}
 );
